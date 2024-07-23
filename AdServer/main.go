701ee687c2a2fc8d31b7f97950627a2fbdff4914
sina <mr.sinaimani@gmail.com>
2024-07-23 14:27:46 +0330

    [FIX]: remove mutex on allAds

--- AdServer/main.go
+++ AdServer/main.go
@@ -8,17 +8,16 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-/* Constants Configuring Functionality of the Server. */
+/* Constants Configuring Functionality of the Server */
 
 var TEST_RAW_RESPONSE = []byte(`[{"Id":1,"Title":"12","ImagePath":"uploads\\treesample.png","BidValue":12,"IsActive":true,"Clicks":0,"Impressions":0,"AdvertiserID":2,"Advertiser":{"Id":0,"Name":"","Credit":0}},{"Id":6,"Title":"144","ImagePath":"media\\treesample.png","BidValue":144,"IsActive":true,"Clicks":0,"Impressions":0,"AdvertiserID":2,"Advertiser":{"Id":0,"Name":"","Credit":0}},{"Id":11,"Title":"test","ImagePath":"media/swoled_20240722144230_2.jpg","BidValue":12,"IsActive":true,"Clicks":0,"Impressions":0,"AdvertiserID":2,"Advertiser":{"Id":0,"Name":"","Credit":0}},{"Id":10,"Title":"first","ImagePath":"media/s.jpg","BidValue":100,"IsActive":true,"Clicks":0,"Impressions":0,"AdvertiserID":2,"Advertiser":{"Id":0,"Name":"","Credit":0}}]`)
 
-const FETCH_PERIOD = 5 // How many minutes to wait between fetching
+const FETCH_PERIOD = 1 // How many minutes to wait between fetching
 // Ads from Panel.
 const FETCH_URL = "http://localhost:8080/api/v1/ads/active/"		// Address from which ads are to be fetched.
 const EVENT_URL = "http://localhost:7070/"						// Address to which ads are to be sent.
@@ -46,7 +45,6 @@ type ResponseInfo struct {
 /* Global Objects. */
 
 var allFetchedAds []FetchedAd		// A slice containing all ads.
-var allAdsMutex sync.Mutex			// Mutex object to syncronize working with allAds.
 
 /* Functions of the Server. */
 
@@ -86,9 +84,6 @@ func fetchAds() error {
 		// Replacing the returned response with a test respone.
 		// TODO: Remove the replacement of response done here.
 		json.Unmarshal(TEST_RAW_RESPONSE, &allFetchedAds)
-		allAdsMutex.Lock()
-		json.Unmarshal(TEST_RAW_RESPONSE, &allFetchedAds)
-		allAdsMutex.Unlock()
 
 		if PRINT_RESPONSE {
 			log.Printf("Successful Ad Fetch.\nallAds: %+v\n", allFetchedAds)
@@ -100,14 +95,12 @@ func selectAd() FetchedAd {
 	var bestAd FetchedAd
 	var maxBid int = 0
 
-	allAdsMutex.Lock()
 	for _, ad := range allFetchedAds {
 		if ad.Bid > maxBid {
 			maxBid = ad.Bid
 			bestAd = ad
 		}
 	}
-	allAdsMutex.Unlock()
 	
 	return bestAd
 }
