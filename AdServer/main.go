980e8bea8a678e76aa8759111c884d9f9ed5a31c
sina <mr.sinaimani@gmail.com>
2024-07-23 13:04:39 +0330

    [ADD]: Mock info fetch from Panel

--- AdServer/main.go
+++ AdServer/main.go
@@ -13,6 +13,8 @@ import (
 
 /* Constants Configuring Functionality of the Server. */
 
+var TEST_RAW_RESPONSE = []byte(`[{"Id":1,"Title":"12","ImagePath":"uploads\\treesample.png","BidValue":12,"IsActive":true,"Clicks":0,"Impressions":0,"AdvertiserID":2,"Advertiser":{"Id":0,"Name":"","Credit":0}},{"Id":6,"Title":"144","ImagePath":"media\\treesample.png","BidValue":144,"IsActive":true,"Clicks":0,"Impressions":0,"AdvertiserID":2,"Advertiser":{"Id":0,"Name":"","Credit":0}},{"Id":11,"Title":"test","ImagePath":"media/swoled_20240722144230_2.jpg","BidValue":12,"IsActive":true,"Clicks":0,"Impressions":0,"AdvertiserID":2,"Advertiser":{"Id":0,"Name":"","Credit":0}},{"Id":10,"Title":"first","ImagePath":"media/s.jpg","BidValue":100,"IsActive":true,"Clicks":0,"Impressions":0,"AdvertiserID":2,"Advertiser":{"Id":0,"Name":"","Credit":0}}]`)
+
 const FETCH_PERIOD = 5 // How many minutes to wait between fetching
 // Ads from Panel.
 const FETCH_URL = "http://localhost:8080/api/v1/ads/active"
@@ -64,14 +66,17 @@ func fetchAds() error {
 			continue
 		}
 
-		responseByte, err := io.ReadAll(resp.Body)
+		_, err = io.ReadAll(resp.Body)
 		if err != nil {
 			log.Print("error in reading response body:", err)
 			continue
 		}
 
+		// Replacing the returned response with a test respone.
+		// TODO: Remove the replacement of response done here.
+		json.Unmarshal(TEST_RAW_RESPONSE, &allAds)
 		allAdsMutex.Lock()
-		json.Unmarshal(responseByte, &allAds)
+		json.Unmarshal(TEST_RAW_RESPONSE, &allAds)
 		allAdsMutex.Unlock()
 
 		if PRINT_RESPONSE {
