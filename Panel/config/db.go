a18077484467ed6f30613c5e41fcd3a2be1a08e9
Mohammad Fallah <fallahmohammadm@gmail.com>
2024-07-25 14:11:24 +0330

    add logger to connector

--- Panel/config/db.go
+++ Panel/config/db.go
@@ -5,13 +5,42 @@ import (
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 	"log"
 	"os"
+	"time"
 )
 
 var DB *gorm.DB
 
 func Connect() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dsn := os.Getenv("DSN")
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			Colorful:                  true,        // Disable color
+		},
+	)
+
+	// Globally mode
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{Logger: newLogger})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	DB = db
+	//return db
+}
+
+func Connect2() {
 	//DSN=host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable
 	err := godotenv.Load()
 	if err != nil {
