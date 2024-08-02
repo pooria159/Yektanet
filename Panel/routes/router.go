104b8643f668287529dd2efc2ad6ed1d0765d751
Mohammad Fallah <fallahmohammadm@gmail.com>
2024-08-02 10:42:06 +0330

    add middleware to panel

--- Panel/routes/router.go
+++ Panel/routes/router.go
@@ -7,8 +7,26 @@ import (
 	"gorm.io/gorm"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func SetupRouter(db *gorm.DB) *gin.Engine {
 	router := gin.Default()
+
+	router.Use(CORSMiddleware())
 	router.LoadHTMLGlob("templates/*")
 	router.Static("/static", "./static")
 	router.Static("/media", "./media") // Serve uploaded files from the media directory
